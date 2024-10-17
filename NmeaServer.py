cc1901ba6719b3b70e0d5430d38973a4067a32be
Аметов С.И <ametov@sicon.ru>
2024-10-17 09:19:21 +0300

    modified:   NmeaServer.py
    	modified:   NmeaServer.py

--- NmeaServer.py
+++ NmeaServer.py
@@ -15,10 +15,6 @@ import keyboard
 IS_WIN = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
 DEFAULT_PORT = 5007
 INTERVAL_TX_PACKET = 1  # sec
-
-if IS_WIN:
-    from msvcrt import getch
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_PATH = f"{BASE_DIR}/nmea.log" if IS_WIN else "/var/log/nmea.log"
 
